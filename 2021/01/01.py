d81c5d6cf96e1cb8cfef4b5fb4a0811b41a4e34e
Marval13 <dbaro13@gmail.com>
2021-12-01 10:21:46 +0100

    day 1

---
+++ 2021/01/01.py
@@ -0,0 +1,21 @@
+with open('input.txt') as file:
+	data = []
+	for l in list(file):
+		data.append(int(l))
+
+sincrements = 0
+for i in range(0, len(data) - 1):
+    if data[i] < data[i + 1]:
+        sincrements += 1
+
+print("Single increments", sincrements)
+
+def window(list, index, length):
+    return sum([list[i] for i in range(index, index + length)])
+
+wincrements = 0
+for i in range(0, len(data) - 3):
+    if window(data, i, 3) < window(data, i + 1, 3):
+        wincrements += 1
+
+print("Triple increments", wincrements)
